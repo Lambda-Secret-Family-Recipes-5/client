59dc09cfd4f0ae397b0b3f5c0aa4b12cafc39244
Ethan-Edmond <eedmond@uccs.edu>
2021-05-24 12:47:53 -0600

    Added basic dashboard structure

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from './Header';
+import Recipe from './Recipe';
 
 export default function Dashboard() {
+  const [recipes, setRecipes] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <div className='dashboard'>
-      
+      <Header searchTerm={searchTerm}/>
+      {
+        recipes.map(recipe => {
+          return (
+            <Recipe
+              category={recipe.category}
+              ingredients={recipe.ingredients}
+              instructions={recipe.instructions}
+              source={recipe.source}
+              title={recipe.title}/>
+          ).filter(recipe => {
+            return recipe.title.contains(searchTerm);
+          });
+        })
+      }
     </div>
   )
 }
