a7f8118f208dd85e6d8a46b2550cfc34e89493cf
Ethan-Edmond <eedmond@uccs.edu>
2021-05-24 20:43:55 -0600

    Added styled-components and centered content in Splash

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import styled from "styled-components";
+
+const SplashContainer = styled.div`
+  background: red;
+  overflow: auto;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
 export default function Splash() {
   return (
-    <div className='splash'>
+    <SplashContainer className='splash'>
       <h1>Secret Recipes</h1>
       <Link to="/signup">Sign Up</Link>
       <Link to="/login">Login</Link>
       <Link to="/recipes">Recipes</Link>
-    </div>
+    </SplashContainer>
   )
 }
