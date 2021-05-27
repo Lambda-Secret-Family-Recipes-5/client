57f0c99da3371200451ac0968eabc588ebc3749f
Ethan-Edmond <eedmond@uccs.edu>
2021-05-27 12:24:19 -0600

    Changed the input structure, got categories working

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -47,49 +47,80 @@ const StyledAddRecipe = styled.div`
 const AddRecipe = (props) => {
 
 	const initialRecipeValues ={
-	id:"",
-	title:"",
-	source: "",
-	ingredients:[{
-				id:Date.now(),
-				name:"", 
-				quantity:"", 
-				unit:""
-			}],
-	description: "",
-	categories: [""],
-	steps: [{step_number:"", 
-			instructions:""
-			}],
-		}
+	  id:"",
+	  title:"",
+	  source: "",
+	  ingredients:[],
+	  description: "",
+	  categories: [],
+	  steps: [],
+	};
 
-   
 	const { push } = useHistory();
 	const { id } = useParams();
 
 	const [recipe, setRecipe] = useState(initialRecipeValues);
-	const [moreIngredients, setMoreIngredients] = useState(false);
-	const [moreSteps, setMoreSteps] = useState(false);
+	const [currIngredient, setCurrIngredient] = useState({
+    id: Date.now(),
+    name: "",
+    quantity: 0,
+    unit: ""
+  });
+	const [currStep, setCurrStep] = useState({
+    step_number: recipe.steps.length,
+    instructions: ""
+  });
+  const [currCategory, setCurrCategory] = useState("");
 
-	
+  const addIngredient = (newIngredient) => {
+    setRecipe({
+      ...recipe,
+      ingredients: [...recipe.ingredients, newIngredient]
+    });
+  };
+  const addStep = (newStepInstructions) => {
+    setRecipe({
+      ...recipe,
+      steps: [...recipe.steps, {
+        step_number: recipe.steps.length,
+        instructions: newStepInstructions
+      }]
+    });
+  };
+  const addCategory = (category) => {
+    setRecipe({
+      ...recipe,
+      categories: [...recipe.categories, category]
+    });
+  };
+
 	useEffect(()=>{
-        axiosWithAuth().post(`/recipes/${id}`)
-            .then(res=>{
-                setRecipe(res.data);
-            })
-            .catch(err=>{
-                console.log(err.res);
-            })
-    }, [id]);
+    axiosWithAuth().get(`/recipes/${id}`)
+      .then(res=>{
+        setRecipe(res.data);
+      })
+      .catch(err=>{
+        console.log(err);
+      });
+  }, [id]);
 
 	const handleChange = (e) => {
-        setRecipe({
-            ...recipe,
-            [e.target.name]: e.target.value
-        });
-    }
+    setRecipe({
+      ...recipe,
+      [e.target.name]: e.target.value
+    });
+  };
 
-    const handleSubmit = (e) => {
+  const handleCategory = (e) => {
+    setCurrCategory(e.target.value);
+  };
+
+  const submitCategory = (e) => {
+    e.preventDefault();
+    addCategory(currCategory);
+  };
+
+  const handleSubmit = (e) => {
 		e.preventDefault();
 
 		const newRecipe = {
@@ -111,144 +142,150 @@ const AddRecipe = (props) => {
       })
 	}
 
-	const toggleIngredients = e => {
-		e.preventDefault();
-		setMoreIngredients(!moreIngredients);
-	  };
-	const toggleSteps = e => {
-		e.preventDefault();
-		setMoreSteps(!moreSteps);
-	  };
-
-    return (
-	<StyledAddRecipe>
-		<div>
-			<form onSubmit={handleSubmit}>
-				<div>						
-					<h1>Adding {recipe.title} Recipe </h1>
-				</div>
-				<div>					
-					<div>
-						<label>Title</label>
-						<input value={recipe.title} onChange={handleChange} name="title" type="text" placeholder="Title"/>
-					</div>
-					<div>
-						<label>Source</label>
-						<input value={recipe.source} onChange={handleChange} name="source" type="text" placeholder="Source"/>
-					</div>
-					<div>
-						<label>Category</label>
-						<input value={recipe.categories} onChange={handleChange} name="categories" type="text" placeholder="Categories" />
-					</div>
-					<div>
-						<label>Description</label>
-						<textarea value={recipe.description} onChange={handleChange} name="description" placeholder="Description"/>
-					</div>		
-					<div>
-						<label>Ingredients</label>
-						<input value={recipe.ingredients.name} onChange={handleChange} name="name" type="text" placeholder="Ingredient Name" />
-						<select value={recipe.ingredients.quantity} name="quantity" onChange={handleChange} >
-            				<option value="">-- Quantity --</option>
-            				<option value="1">1</option>
-            				<option value="2">2</option>
-            				<option value="3">3</option>
-							<option value="4">4</option>
-            				<option value="5">5</option>
-            				<option value="6">6</option>
-							<option value="7">7</option>
-            				<option value="8">8</option>
-            				<option value="9">9</option>
-          				</select>
+  console.log(recipe);
+  return (
+	  <StyledAddRecipe>
+		  <div>
+			  <form onSubmit={handleSubmit}>
+				  <div>
+					  <h1>Adding Recipe</h1>
+				  </div>
+				  <div>
+					  <div>
+						  <label>Title</label>
+						  <input value={recipe.title} onChange={handleChange} name="title" type="text" placeholder="Title"/>
+					  </div>
+					  <div>
+						  <label>Source</label>
+						  <input value={recipe.source} onChange={handleChange} name="source" type="text" placeholder="Source"/>
+					  </div>
+					  <div>
+              {
+                (recipe.categories.length !== 0) &&
+                  <>
+                  <h4>Current Categories: </h4>
+                    <ul>
+                      { recipe.categories.map(category => {
+                        return (
+                          <li>{category}</li>
+                        );
+                      })}
+                    </ul>
+                  </>
+              }
+						  <label>Add Category</label>
+						  <input value={currCategory} onChange={handleCategory} name="categories" type="text" placeholder="Categories" />
+              <button onClick={submitCategory}>Add Category</button>
+					  </div>
+					  <div>
+						  <label>Description</label>
+						  <textarea value={recipe.description} onChange={handleChange} name="description" placeholder="Description"/>
+					  </div>		
+					  <div>
+						  <label>Ingredients</label>
+						  <input value={recipe.ingredients.name} onChange={handleChange} name="name" type="text" placeholder="Ingredient Name" />
+						  <select value={recipe.ingredients.quantity} name="quantity" onChange={handleChange} >
+            		<option value="">-- Quantity --</option>
+            		<option value="1">1</option>
+            		<option value="2">2</option>
+            		<option value="3">3</option>
+							  <option value="4">4</option>
+            		<option value="5">5</option>
+            		<option value="6">6</option>
+							  <option value="7">7</option>
+            		<option value="8">8</option>
+            		<option value="9">9</option>
+          		</select>
 						  <select value={recipe.ingredients.unit} name="unit" onChange={handleChange} >
-            				<option value="">-- Unit --</option>
-            				<option value="cup">Cup</option>
-            				<option value="package">Package</option>
-							<option value="cube">Cube</option>
-							<option value="sachet">Sachet</option>
-							<option value="cup">Cup</option>
-          				</select>s
-						{/* <input value={ingredients} onChange={handleChange} name="quantity" type="text" placeholder="quantity"/>
-						<input value={ingredients} onChange={handleChange} name="unit" type="text" placeholder="unit"/>s */}
-					</div><br/>
-					<div>
-					{ moreIngredients && <>
-					  <input value={recipe.name} onChange={handleChange} name="name" type="text" placeholder="Ingredient Name" />
-					  <select value={recipe.ingredients.quantity} name="quantity" onChange={handleChange}>
-            				<option value="">-- Quantity --</option>
-            				<option value="1">1</option>
-            				<option value="2">2</option>
-            				<option value="3">3</option>
-							<option value="4">4</option>
-            				<option value="5">5</option>
-            				<option value="6">6</option>
-							<option value="7">7</option>
-            				<option value="8">8</option>
-            				<option value="9">9</option>
-          				</select>
-						  <select value={recipe.ingredients.unit} name="unit" onChange={handleChange} >
-            				<option value="">-- Unit --</option>
-            				<option value="cup">Cup</option>
-            				<option value="package">Package</option>
-							<option value="cube">Cube</option>
-							<option value="sachet">Sachet</option>
-							<option value="cup">Cup</option>
-          				</select>s
-						{/* <input value={ingredients} onChange={handleChange} name="quantity" type="text" placeholder="quantity"/>
-						<input value={ingredients} onChange={handleChange} name="unit" type="text" placeholder="unit"/>s */}
-						</>
-						}</div>
-					<div>
-					<Link onClick={toggleIngredients}>Add New Ingredients</Link>
-					</div>	<br/>
-					<div>
-						<label>Steps</label>
-						<select value={recipe.steps.step_number} name="step_number" onChange={handleChange} >
-            				<option value="">-- Steps --</option>
-            				<option value="1">1</option>
-            				<option value="2">2</option>
-            				<option value="3">3</option>
-							<option value="4">4</option>
-            				<option value="5">5</option>
-            				<option value="6">6</option>
-							<option value="7">7</option>
-            				<option value="8">8</option>
-            				<option value="9">9</option>
-          				</select><br/>
-						{/* <input value={steps} onChange={handleChange} name="step_number" type="text" placeholder="Steps" /><br/> */}
-						<textarea value={recipe.steps.instructions} onChange={handleChange} name="instructions" type="text" placeholder="Instructions"/>
-					</div>	
-					 <div>
-						{ moreSteps ? <>
-							<select value={recipe.steps.step_number} name="step_number" onChange={handleChange} >
-            				<option value="">-- Steps --</option>
-            				<option value="1">1</option>
-            				<option value="2">2</option>
-            				<option value="3">3</option>
-							<option value="4">4</option>
-            				<option value="5">5</option>
-            				<option value="6">6</option>
-							<option value="7">7</option>
-            				<option value="8">8</option>
-            				<option value="9">9</option>
-          				</select><br/>
-							{/* <input value={steps} onChange={handleChange} name="step_number" type="text" placeholder="Steps" /><br/> */}
-							<textarea value={recipe.steps.instructions} onChange={handleChange} name="instructions" type="text" placeholder="Instructions"/>
-							</>
-							: ""}
+            		<option value="">-- Unit --</option>
+            		<option value="cup">Cup</option>
+            		<option value="package">Package</option>
+							  <option value="cube">Cube</option>
+							  <option value="sachet">Sachet</option>
+							  <option value="cup">Cup</option>
+          		</select>s
+						  {/* <input value={ingredients} onChange={handleChange} name="quantity" type="text" placeholder="quantity"/>
+						     <input value={ingredients} onChange={handleChange} name="unit" type="text" placeholder="unit"/>s */}
+					  </div><br/>
+					  <div>
+					    { /* moreIngredients && <> */
+					      /*   <input value={recipe.name} onChange={handleChange} name="name" type="text" placeholder="Ingredient Name" /> */
+					      /*   <select value={recipe.ingredients.quantity} name="quantity" onChange={handleChange}> */
+            		/* 		<option value="">-- Quantity --</option> */
+            		/* 		<option value="1">1</option> */
+            		/* 		<option value="2">2</option> */
+            		/* 		<option value="3">3</option> */
+							  /*     <option value="4">4</option> */
+            		/* 		<option value="5">5</option> */
+            		/* 		<option value="6">6</option> */
+							  /*     <option value="7">7</option> */
+            		/* 		<option value="8">8</option> */
+            		/* 		<option value="9">9</option> */
+          			/* 	</select> */
+						    /*   <select value={recipe.ingredients.unit} name="unit" onChange={handleChange} > */
+            		/* 		<option value="">-- Unit --</option> */
+            		/* 		<option value="cup">Cup</option> */
+            		/* 		<option value="package">Package</option> */
+							  /*     <option value="cube">Cube</option> */
+							  /*     <option value="sachet">Sachet</option> */
+							  /*     <option value="cup">Cup</option> */
+          			/* 	</select>s */
+						    /*   {/\* <input value={ingredients} onChange={handleChange} name="quantity" type="text" placeholder="quantity"/> */
+						    /*      <input value={ingredients} onChange={handleChange} name="unit" type="text" placeholder="unit"/>s *\/} */
+						    /* </> */
+						  }</div>
+					  <div>
+					    {/* <Link onClick={toggleIngredients}>Add New Ingredients</Link> */}
+					  </div>	<br/>
+					  <div>
+						  <label>Steps</label>
+						  <select value={recipe.steps.step_number} name="step_number" onChange={handleChange} >
+            		<option value="">-- Steps --</option>
+            		<option value="1">1</option>
+            		<option value="2">2</option>
+            		<option value="3">3</option>
+							  <option value="4">4</option>
+            		<option value="5">5</option>
+            		<option value="6">6</option>
+							  <option value="7">7</option>
+            		<option value="8">8</option>
+            		<option value="9">9</option>
+          		</select><br/>
+						  {/* <input value={steps} onChange={handleChange} name="step_number" type="text" placeholder="Steps" /><br/> */}
+						  <textarea value={recipe.steps.instructions} onChange={handleChange} name="instructions" type="text" placeholder="Instructions"/>
+					  </div>	
+					  <div>
+						  {/* { moreSteps ? <> */}
+							{/*                 <select value={recipe.steps.step_number} name="step_number" onChange={handleChange} > */}
+            	{/* 			            <option value="">-- Steps --</option> */}
+            	{/* 			            <option value="1">1</option> */}
+            	{/* 			            <option value="2">2</option> */}
+            	{/* 			            <option value="3">3</option> */}
+							{/*                   <option value="4">4</option> */}
+            	{/* 			            <option value="5">5</option> */}
+            	{/* 			            <option value="6">6</option> */}
+							{/*                   <option value="7">7</option> */}
+            	{/* 			            <option value="8">8</option> */}
+            	{/* 			            <option value="9">9</option> */}
+          		{/* 		            </select><br/> */}
+							{/*                 {/\* <input value={steps} onChange={handleChange} name="step_number" type="text" placeholder="Steps" /><br/> *\/} */}
+							{/*                 <textarea value={recipe.steps.instructions} onChange={handleChange} name="instructions" type="text" placeholder="Instructions"/> */}
+							{/*               </> */}
+							{/*   : ""} */}
 						</div>
 						<div>
-						<Link onClick={toggleSteps}>Add More Steps</Link>
+						  {/* <Link onClick={toggleSteps}>Add More Steps</Link> */}
 						</div>	
-				</div>
-                <br/>
-				<div>			    
-					<button type="submit" value="Save">Submit</button>
-					<Link to={`/recipes/`}><button type="button"  value="Cancel"> Cancel </button></Link>
-				</div>
-			</form>
-		</div>
-	</StyledAddRecipe>
-    );
+				  </div>
+          <br/>
+				  <div>			    
+					  <button type="submit" value="Save">Submit</button>
+					  <Link to={`/recipes/`}><button type="button"  value="Cancel"> Cancel </button></Link>
+				  </div>
+			  </form>
+		  </div>
+	  </StyledAddRecipe>
+  );
 }
 
 export default AddRecipe;
